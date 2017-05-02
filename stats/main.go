00ae71fa46d4892abeb6cb374dc71fb576928cd6
vrg <vrgakos@gmail.com>
2017-05-02 15:42:25 +0200

    Stats

--- stats/main.go
+++ stats/main.go
@@ -8,21 +8,36 @@ import (
 	"log"
 	"os"
 	"os/signal"
+	"flag"
+	"path/filepath"
 )
 
+var fileName string
+var interval time.Duration
+
+func init() {
+	flag.StringVar(&fileName, "file", "stats.xlsx", "Output file path")
+	flag.DurationVar(&interval, "interval", time.Millisecond * 100, "Measure interval")
+}
+
 func main() {
+	flag.Parse()
+	dir, _ := filepath.Abs(filepath.Dir(fileName))
+	log.Println("Create directory", dir)
+	os.MkdirAll(dir, 0777)
 
 	var file *xlsx.File
-	var sheet *xlsx.Sheet
+	var sheetCpu, sheetMem *xlsx.Sheet
+	var netSheets []*xlsx.Sheet
+	var diskSheets []*xlsx.Sheet
 	var row *xlsx.Row
 	var err error
 
 
 	file = xlsx.NewFile()
-	sheet, err = file.AddSheet("Sheet1")
-	if err != nil {
-		fmt.Printf(err.Error())
-	}
+	sheetCpu, _ = file.AddSheet("CPU")
+	sheetMem, _ = file.AddSheet("MEM")
+
 	log.Println("Start")
 
 
@@ -36,33 +51,119 @@ func main() {
 		}
 	}()
 
-	row = sheet.AddRow()
+	// CPU
+	row = sheetCpu.AddRow()
 	row.AddCell().SetValue("Time (s:ms)")
 	row.AddCell().SetValue("User")
 	row.AddCell().SetValue("Kernel")
 	//row.AddCell().SetValue("Idle")
 	row.AddCell().SetValue("IOWait")
 
+	// MEM
+	row = sheetMem.AddRow()
+	row.AddCell().SetValue("Time (s:ms)")
+	row.AddCell().SetValue("Used")
+	row.AddCell().SetValue("Free")
+	row.AddCell().SetValue("Cache")
+
+	// NET
+	s := statgo.NewStat()
+	for _, stat := range s.NetIOStats() {
+		sheet, _ := file.AddSheet("NET-" + stat.IntName)
+		netSheets = append(netSheets, sheet)
+
+		row = sheet.AddRow()
+		row.AddCell().SetValue("Time (s:ms)")
+		row.AddCell().SetValue("RX (MB/s)")
+		row.AddCell().SetValue("TX (MB/s)")
+		row.AddCell().SetValue("IPackets (packets/s)")
+		row.AddCell().SetValue("OPackets (packets/s)")
+		row.AddCell().SetValue("seconds")
+	}
+
+	// DISK
+	for _, stat := range s.DiskIOStats() {
+		sheet, _ := file.AddSheet("DISK-" + stat.DiskName)
+		diskSheets = append(diskSheets, sheet)
+
+		row = sheet.AddRow()
+		row.AddCell().SetValue("Time (s:ms)")
+		row.AddCell().SetValue("Read (MB/s)")
+		row.AddCell().SetValue("Write (MB/s)")
+		row.AddCell().SetValue("seconds")
+	}
+
+
+
 
 	start := time.Now().UnixNano()
+	last := time.Now()
 	for run {
-		row = sheet.AddRow()
+		timer := time.NewTimer(interval)
 
-		row.AddCell().SetString(fmt.Sprintf("%.2f", (time.Now().UnixNano() - start) / 1000000000))
+		diff := time.Now().Sub(last)
+		last = time.Now()
+		secs := float64(diff.Nanoseconds()) / float64(1000000000)
+
+		timeString := fmt.Sprintf("%.1f", float64(time.Now().UnixNano() - start) / float64(1000000000))
+
 
 		s := statgo.NewStat()
 
-		row.AddCell().SetValue(s.CPUStats().User)
-		row.AddCell().SetValue(s.CPUStats().Kernel)
+		// CPU STATS
+		cpuStats := s.CPUStats()
+		row = sheetCpu.AddRow()
+		row.AddCell().SetString(timeString)
+		row.AddCell().SetFloat(cpuStats.User)
+		row.AddCell().SetFloat(cpuStats.Kernel)
 		//row.AddCell().SetValue(s.CPUStats().Idle)
-		row.AddCell().SetValue(s.CPUStats().IOWait)
-		//row.AddCell().SetValue(times[0].String())
+		row.AddCell().SetFloat(cpuStats.IOWait)
 
-		time.Sleep(time.Millisecond * 100)
+
+		// MEM STATS
+		memStats := s.MemStats()
+		row = sheetMem.AddRow()
+		row.AddCell().SetString(timeString)
+		row.AddCell().SetFloat(float64(memStats.Used) / 1024 / 1024)
+		row.AddCell().SetFloat(float64(memStats.Free) / 1024 / 1024)
+		row.AddCell().SetFloat(float64(memStats.Cache) / 1024 / 1024)
+
+
+		// NET STATS
+		for i, stat := range s.NetIOStats() {
+			sheet := netSheets[i]
+
+			if stat.Period.Seconds() > 0 {
+				row = sheet.AddRow()
+				secs = stat.Period.Seconds()
+				row.AddCell().SetString(timeString)
+				row.AddCell().SetFloat((float64(stat.RX) / 1024 / 1024) / secs)
+				row.AddCell().SetFloat((float64(stat.TX) / 1024 / 1024) / secs)
+				row.AddCell().SetFloat(float64(stat.IPackets) / secs)
+				row.AddCell().SetFloat(float64(stat.OPackets) / secs)
+				row.AddCell().SetFloat(secs)
+			}
+		}
+
+		// DISK STATS
+		for i, stat := range s.DiskIOStats() {
+			sheet := diskSheets[i]
+
+			if stat.Period.Seconds() > 0 {
+				row = sheet.AddRow()
+				secs = stat.Period.Seconds()
+				row.AddCell().SetString(timeString)
+				row.AddCell().SetFloat(float64(stat.ReadBytes) / 1024 / 1024 / secs)
+				row.AddCell().SetFloat(float64(stat.WriteBytes) / 1024 / 1024 / secs)
+				row.AddCell().SetFloat(secs)
+			}
+		}
+
+		<-timer.C
 	}
 
 	log.Println("Received an interrupt, saving")
-	err = file.Save("asd.xlsx")
+	err = file.Save(fileName)
 	if err != nil {
 		fmt.Printf(err.Error())
 	}
